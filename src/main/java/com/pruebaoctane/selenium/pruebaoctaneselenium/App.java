ce9e405e8a7ad500c1ebc144ad46c7aafecb7ba2
AlexisRicardoG <AlexisRicardoG@hexaware.com>
2021-09-15 15:39:58 -0500

    se agrega el proyecto de selenium

---
+++ src/main/java/com/pruebaoctane/selenium/pruebaoctaneselenium/App.java
@@ -0,0 +1,13 @@
+package com.pruebaoctane.selenium.pruebaoctaneselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/pruebaoctane/selenium/pruebaoctaneselenium/GoogleSearch.java
@@ -0,0 +1,44 @@
+package com.pruebaoctane.selenium.pruebaoctaneselenium;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void setUp(){
+		
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		//driver.execute_script("replace.window.location{"www.Url2.com"}");
+	}
+	@Test
+	public void testGooglePage(){
+ 		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.clear();
+		searchBox.sendKeys("Hexaware");
+		searchBox.submit();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		assertEquals("Hexaware - Buscar con Google", driver.getTitle());
+	}
+	@After
+	public void tearDown(){
+		driver.quit();
+	}
+}
